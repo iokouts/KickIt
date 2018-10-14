6a4db02b9dde16f1597ed5a92b64b061228341aa
iokouts <iokouts@gmail.com>
2018-10-15 00:15:32 +0300

    added Author Page Model and templates

--- home/models.py
+++ home/models.py
@@ -2,12 +2,8 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import (FieldPanel, FieldRowPanel, MultiFieldPanel)
+from wagtail.admin.edit_handlers import (FieldPanel, MultiFieldPanel)
 from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.search import index
-from wagtail.snippets.models import register_snippet
-
-from modelcluster.models import ClusterableModel
 
 from blog.models import BlogPage
 
@@ -33,66 +29,8 @@ class HomePage(Page):
 
     def get_context(self, request):
         context = super(HomePage, self).get_context(request)
-        fposts = BlogPage.objects.all()
-        context['featured_posts'] = fposts
+        featured_posts = BlogPage.objects.live().order_by('-first_published_at')[:2]
+        context['featured_posts'] = featured_posts
 
         return context
 
-
-@register_snippet
-class People(index.Indexed, ClusterableModel):
-    """
-    A Django model to store People objects.
-    It uses the `@register_snippet` decorator to allow it to be accessible
-    via the Snippets UI (e.g. /admin/snippets/base/people/)
-
-    `People` uses the `ClusterableModel`, which allows the relationship with
-    another model to be stored locally to the 'parent' model (e.g. a PageModel)
-    until the parent is explicitly saved. This allows the editor to use the
-    'Preview' button, to preview the content, without saving the relationships
-    to the database.
-    https://github.com/wagtail/django-modelcluster
-    """
-    first_name = models.CharField("First name", max_length=254)
-    last_name = models.CharField("Last name", max_length=254)
-    # job_title = models.CharField("Job title", max_length=254)
-
-    image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    panels = [
-        MultiFieldPanel([
-            FieldRowPanel([
-                FieldPanel('first_name', classname="col6"),
-                FieldPanel('last_name', classname="col6"),
-            ])
-        ], "Name"),
-        # FieldPanel('job_title'),
-        ImageChooserPanel('image')
-    ]
-
-    search_fields = [
-        index.SearchField('first_name'),
-        index.SearchField('last_name'),
-    ]
-
-    @property
-    def thumb_image(self):
-        # Returns an empty string if there is no profile pic or the rendition
-        # file can't be found.
-        try:
-            return self.image.get_rendition('fill-50x50').img_tag()
-        except:
-            return ''
-
-    def __str__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
-
-    class Meta:
-        verbose_name = 'Person'
-        verbose_name_plural = 'People'
