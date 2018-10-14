74833496fc474b022d0397be6f5406d862508b46
iokouts <iokouts@gmail.com>
2018-10-14 19:19:36 +0300

    mass rename

--- KickIt/wsgi.py
+++ KickIt/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for KickTestSite project.
+WSGI config for KickIt project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KickTestSite.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KickIt.settings.dev")
 
 application = get_wsgi_application()
