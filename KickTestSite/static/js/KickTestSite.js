c6ef0ba8ae1ce2b78a64e7995906c014378e13f3
iokouts <iokouts@gmail.com>
2018-10-14 18:34:43 +0300

    First commit

---
+++ KickTestSite/static/js/KickTestSite.js
@@ -0,0 +1,26 @@
+var stickymenu;
+var stickymenuoffset;
+var scrolltimer;
+var logo_image;
+
+window.onload = function() {
+    stickymenu = document.getElementById("sticky_menu");
+    stickymenuoffset = stickymenu.offsetTop + 200;
+
+    //logo_image = document.getElementById("logo_path");//.getElementsByTagName("path");
+    logo_image = $("#logo_path");
+}
+
+window.addEventListener("scroll", function(e){
+    requestAnimationFrame(function(){
+        if (window.pageYOffset > stickymenuoffset){
+            stickymenu.classList.add('sticky');
+            logo_image.css("fill","#fff");
+        }
+        else{
+            stickymenu.classList.remove('sticky');
+            logo_image.css("fill","#000");
+        }
+    })
+})
+
