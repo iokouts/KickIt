96bbc509e4123ac76beebc91b5f26a4068d13274
Giorgos Logiotatidis <seadog@sealabs.net>
2018-11-18 00:11:52 +0200

    12factorize settings.

--- KickIt/wsgi/__init__.py
+++ KickIt/wsgi/__init__.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KickIt.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KickIt.settings.base")
 
 application = get_wsgi_application()
