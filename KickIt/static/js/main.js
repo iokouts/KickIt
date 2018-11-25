3c9765adc7e225d28b15025401bb4cfee7a348f9
iokouts <iokouts@gmail.com>
2018-11-25 21:56:41 +0200

    hamburger menu

--- KickIt/static/js/main.js
+++ KickIt/static/js/main.js
@@ -31,3 +31,20 @@ function resetBackground(x){
     (screen.width <= 500) ? color = '#f7f6f6' : color='#fff';
     x.style.background = color;
 }
+
+function hamburgerButtonClick(){
+    var hamburger = $('.hamburger--spin');
+    var menu = $('.menu');
+
+    var is_active = 'is-active';
+    var mobile_menu = 'mobile_menu';
+
+    if(!hamburger.hasClass(is_active)) {
+        hamburger.addClass(is_active);
+        menu.addClass(mobile_menu);
+    }
+    else {
+        hamburger.removeClass([is_active, mobile_menu]);
+        menu.removeClass(mobile_menu);
+    }
+}
