c377cb2b2e3fa010fe1796066c9eca13b3678903
Giorgos Logiotatidis <seadog@sealabs.net>
2018-11-27 22:23:08 +0200

    Add media files serving.

--- KickIt/wsgi/__init__.py
+++ KickIt/wsgi/__init__.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+
+from whitenoise import WhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KickIt.settings.base")
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
+
+# Add media files
+if settings.MEDIA_ROOT and settings.MEDIA_URL:
+    application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
