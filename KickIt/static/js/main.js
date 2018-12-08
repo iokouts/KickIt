ec47d40b94f6c947960f5c20cee4dd246a353c24
iokouts <iokouts@gmail.com>
2018-12-08 23:58:55 +0200

    responsive almost ready

--- KickIt/static/js/main.js
+++ KickIt/static/js/main.js
@@ -1,25 +1,21 @@
-var stickymenu;
-var stickymenuoffset;
-var logo_image;
+$(function(){
+    var stickymenu = $('#sticky_menu');
+    var stickymenuoffset = stickymenu.offset().top + 5;
+    var logo_image = $('#logo_path');
 
-window.onload = function() {
-    stickymenu = document.getElementById("sticky_menu");
-    stickymenuoffset = stickymenu.offsetTop + 200;
+    var pathname = window.location.pathname; // Returns path only
 
-    logo_image = $("#logo_path");
-};
-
-window.addEventListener("scroll", function(e){
-    requestAnimationFrame(function(){
-        if (window.pageYOffset > stickymenuoffset){
-            stickymenu.classList.add('sticky');
-            logo_image.css("fill","#fff");
+    $(window).scroll(function () {
+        if($(window).scrollTop() > stickymenuoffset) {
+            stickymenu.addClass('sticky');
+                logo_image.css("fill", "#fff");
+        } else {
+            stickymenu.removeClass('sticky');
+            if(pathname === '/' && screen.width > 725) {
+                logo_image.css("fill", "#000");
+            }
         }
-        else{
-            stickymenu.classList.remove('sticky');
-            logo_image.css("fill","#000");
-        }
-    })
+    });
 });
 
 function changeBackground(x, color){
@@ -33,7 +29,7 @@ function resetBackground(x){
 }
 
 function hamburgerButtonClick(){
-    var hamburger = $('.hamburger--spin');
+    var hamburger = $('.hamburger');
     var menu = $('.menu');
 
     var is_active = 'is-active';
@@ -44,7 +40,8 @@ function hamburgerButtonClick(){
         menu.addClass(mobile_menu);
     }
     else {
-        hamburger.removeClass([is_active, mobile_menu]);
+        hamburger.removeClass(is_active);
+        hamburger.removeClass(mobile_menu);
         menu.removeClass(mobile_menu);
     }
 }
