b0f5081845764492080dd055ab12d0e11b05229c
Giannis Koutsoukos <iokouts@gmail.com>
2022-11-02 19:57:56 +0200

    remove import

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, PageChooserPanel
-from wagtail.images.edit_handlers import FieldPanel
 from wagtail.snippets.models import register_snippet
 from wagtail.search import index
 
